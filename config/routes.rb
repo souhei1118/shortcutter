eb2cc94983c46a616a016e07d0dac562c8f4ab31
souhei1118 <so.takashi1118@gmail.com>
2022-05-18 04:29:30 +0000

    [add]ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # ゲストユーザーログイン時ルーティング処理
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  
+
   # ユーザー側のルーティング
   scope module: :public do
 
